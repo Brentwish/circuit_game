87f4eee11336c60b3ea39a2fd98ab8f633b35ad5
brent <brent.wish@gmail.com>
2016-03-29 12:02:50 -0700

    Add basic table gen

--- game.js
+++ game.js
@@ -1,2 +1,17 @@
-var $div = $('<div />').appendTo('body');
-$div.attr('id', 'first_div');
+var $table = $('<table />').appendTo('body');
+$table.attr('id', 'board');
+
+var board_height = 3
+var board_width = 5
+var jboard = '#board'
+
+$(jboard).empty();
+for (var i = 0; i < board_height; i++) {
+  var tr = $('<tr>');
+  for (var j = 0; j < board_width; j++) {
+    var td = $('<td>')
+      .attr('name', String(i) + "," + String(j))
+    tr.append(td);
+  }
+  $(jboard).append(tr);
+}
