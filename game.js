5d282cf1bdaf5b28e6fb4a5f622d16cdbc74de7c
brent <brent.wish@gmail.com>
2016-03-30 13:21:12 -0700

    Add tile class and tile generation method

--- game.js
+++ game.js
@@ -1,11 +1,46 @@
-var $table = $('<table />').appendTo('body');
-$table.attr('id', 'board');
+
+function tile(x, y, val) {
+  this.x = x;
+  this.y = y;
+  this.val = val;
+}
+
+tile.prototype.is_hall = function() {
+  return this.val == 1;
+}
+
+tile.prototype.is_wall = function() {
+  return this.val == 0;
+}
+
+tile.prototype.same_coords = function(coords) {
+  return this.x == coords.x && this.y == coords.y;
+}
+
+tile.prototype.toString = function() {
+  return String(this.x) + "," + String(this.y);
+}
 
 board = {
-  width        : 5,
-  height       : 3,
+  width : 5,
+  height : 3,
+  tiles : new Array(),
   player_coord : { x: 0, y: 0 },
-  draw         : function() {
+  get_tile_at : function(x, y) {
+    return board.tiles[y][x]
+  },
+  generate : function() {
+    var $table = $('<table />').appendTo('body');
+    $table.attr('id', 'board');
+    for (i = 0; i < board.height; i++) {
+      board.tiles[i] = new Array();
+      for (j = 0; j < board.width; j++) {
+        board.tiles[i][j] = new tile(j, i, 0);
+      }
+    }
+    board.draw()
+  },
+  draw : function() {
     $('#board').empty()
     for (var i = 0; i < board.height; i++) {
       var tr = $('<tr>')
@@ -19,4 +54,5 @@ board = {
     }
   }
 }
-board.draw()
+
+board.generate()
