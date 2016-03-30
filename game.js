ff3da611fdf60582bc1e0c884ba39a5da5764188
brent <brent.wish@gmail.com>
2016-03-30 10:56:18 -0700

    Create board class

--- game.js
+++ game.js
@@ -1,21 +1,22 @@
 var $table = $('<table />').appendTo('body');
 $table.attr('id', 'board');
 
-var board_height = 3
-var board_width = 5
-var jboard = '#board'
-
-var draw_board = function() {
-  $(jboard).empty()
-  for (var i = 0; i < board_height; i++) {
-    var tr = $('<tr>')
-    for (var j = 0; j < board_width; j++) {
-      var td = $('<td>')
-        .attr('name', String(i) + "," + String(j))
-        .addClass("tile")
-      tr.append(td)
+board = {
+  width        : 5,
+  height       : 3,
+  player_coord : { x: 0, y: 0 },
+  draw         : function() {
+    $('#board').empty()
+    for (var i = 0; i < board.height; i++) {
+      var tr = $('<tr>')
+      for (var j = 0; j < board.width; j++) {
+        var td = $('<td>')
+          .attr('name', String(i) + "," + String(j))
+          .addClass("tile")
+        tr.append(td)
+      }
+      $('#board').append(tr)
     }
-    $(jboard).append(tr)
   }
 }
-draw_board()
+board.draw()
