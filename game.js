b0b70503921665823888f44e131c8c19204baf59
brent <brent.wish@gmail.com>
2016-03-30 10:18:54 -0700

    Wrap board drawing code into a function

--- game.js
+++ game.js
@@ -5,14 +5,17 @@ var board_height = 3
 var board_width = 5
 var jboard = '#board'
 
-$(jboard).empty();
-for (var i = 0; i < board_height; i++) {
-  var tr = $('<tr>');
-  for (var j = 0; j < board_width; j++) {
-    var td = $('<td>')
-      .attr('name', String(i) + "," + String(j))
-      .addClass("tile")
-    tr.append(td);
+var draw_board = function() {
+  $(jboard).empty()
+  for (var i = 0; i < board_height; i++) {
+    var tr = $('<tr>')
+    for (var j = 0; j < board_width; j++) {
+      var td = $('<td>')
+        .attr('name', String(i) + "," + String(j))
+        .addClass("tile")
+      tr.append(td)
+    }
+    $(jboard).append(tr)
   }
-  $(jboard).append(tr);
 }
+draw_board()
